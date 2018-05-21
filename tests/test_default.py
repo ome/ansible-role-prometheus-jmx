b23c7dcd6c38845604fa5b0d215ff0e97acc3a46
Simon Li <spli@dundee.ac.uk>
2018-05-21 13:52:32 +0100

    Run a simple java app to test jmx agent

--- tests/test_default.py
+++ tests/test_default.py
@@ -4,5 +4,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_prometheus_metrics(File):
-    assert File('/opt/prometheus/jars/jmx_prometheus_javaagent.jar').exists
+def test_prometheus_metrics(Command):
+    out = Command.check_output('curl http://localhost:12345/metrics')
+    assert 'process_cpu_seconds_total' in out
